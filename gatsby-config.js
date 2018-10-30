c21d1c3ffbd60f31bdeea777a988496c01dd5839
ilovetraffic <aidarfreelancer@gmail.com>
2018-10-30 22:47:19 +0300

    new

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,14 +29,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-128204944-2",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        head: true,
       },
     },
     {
