2c6a3ba379027feb8ce44eb23192a814c2dc0b07
ilovetraffic <aidarfreelancer@gmail.com>
2018-10-30 22:12:48 +0300

    new

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,28 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-128204944-2",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-yandex-metrika`,
+      options: {
+        trackingId: '50936816',
+        webvisor: true,
+        trackHash: true,
+      },
+    },
     'gatsby-plugin-offline',
     {
       resolve: `gatsby-source-contentful`,
